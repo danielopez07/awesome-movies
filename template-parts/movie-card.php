b7c2746c5bc415777a31338bd69c2ca10bafd4ac
Daniel Lopez <danielopez07@gmail.com>
2020-06-22 11:42:23 -0300

    Refining movie cards

--- template-parts/movie-card.php
+++ template-parts/movie-card.php
@@ -6,28 +6,32 @@
 </div> -->
 
 <article class="card">
-    <a href="<?php echo esc_url( get_permalink() ) ?> ">
+  <a href="<?php echo esc_url( get_permalink() ) ?> ">
     <?php
-        $img = get_post_meta( get_the_ID(), 'movie_poster' )[0];
-        if ( $img )
-            echo '<figure class="card-image"><img src="' . $img . '" /></figure>';
-        else {
-            echo '<figure class="card-image"><img loading="lazy" src="/wp-content/themes/awesome-movies/imgs/default-img.jpg"></figure>';
-        }
+    $img = get_post_meta( get_the_ID(), 'movie_poster' )[0];
+    if ( $img )
+    echo '<figure class="card-image"><img src="' . $img . '" /></figure>';
+    else {
+      echo '<figure class="card-image"><img loading="lazy" src="/wp-content/themes/awesome-movies/imgs/default-img.jpg"></figure>';
+    }
     ?>
-    </a>
 
     <div class="content-card">
-        <a href="<?php echo esc_url( get_permalink() ) ?> ">
-            <h3><?php the_title() ?></h3>
-        </a>
-        <div class="release-date-card">
-          <p>Release date: <?php echo date( 'jS \of F, Y', strtotime( get_post_meta( get_the_ID(), 'release_date' )[0] ) ); ?></p>
-        </div>
-        <!-- <a href="<?php echo esc_url( get_permalink() ) ?> ">
-            <div class="excerpt-card">
-                <?php the_excerpt(); ?>
-            </div>
-        </a> -->
+      <h3><?php the_title() ?></h3>
+
+      <p class="release-date-card">
+        Release date: <?php echo date( 'jS \of F, Y', strtotime( get_post_meta( get_the_ID(), 'release_date' )[0] ) ); ?>
+      </p>
+
+      <div class="genres-card">
+        <?php
+        $movie_genres = json_decode( get_post_meta( get_the_ID(), 'movie_genre' )[0] );
+        foreach ( $movie_genres as $genre ) {
+          echo '<span class="genre">' . $genre->name . '</span>';
+        }
+        ?>
+      </div>
     </div>
+
+  </a>
 </article>
