4f23ec499bfb61ef39226aeadc132036b843f3ba
Daniel Lopez <danielopez07@gmail.com>
2020-06-22 01:07:25 -0300

    Gettinig 10 upcoming movies with details, photos, etc

--- inc/themoviedb-cron.php
+++ inc/themoviedb-cron.php
@@ -2,32 +2,26 @@
 add_action( 'themoviedb_cron', 'themoviedb_cron_func' );
 
 
-function themoviedb_cron_func() {
-  $myfile = fopen( "response.json", "w" ) or die( "Unable to open file!" );
-  fwrite( $myfile, '' );
-  fclose( $myfile );
-  chmod( 'response.json', 0777 );
+define( 'API_URL', 'https://api.themoviedb.org/3/' );
+define( 'API_KEY', 'dd749361bed88e6bbe29fb3e5396489c' );
 
-  $url = "https://api.themoviedb.org/3/configuration?api_key=dd749361bed88e6bbe29fb3e5396489c";
+
+function themoviedb_cron_func() {
+  $url = API_URL . "configuration?api_key=" . API_KEY;
   $config = fetch_API( $url );
 
-  $url = "https://api.themoviedb.org/3/movie/upcoming?api_key=dd749361bed88e6bbe29fb3e5396489c&language=en-US&page=1&region=US";
-  $upcomming = fetch_API( $url );
-  if ( empty( $upcomming ) || ( isset($upcomming->status_code) ) ) {
+  $url = API_URL . "movie/upcoming?api_key=" . API_KEY . "&language=en-US&page=1&region=US";
+  $upcoming = fetch_API( $url );
+  if ( empty( $upcoming ) || ( isset($upcoming->status_code) ) ) {
     return false;
   }
-  else if ( $upcomming->total_results > 10 ) {
-    $upcomming = array_slice( $upcomming->results, 0, 2 );
-    // $upcomming = array_slice( $upcomming->results, 0, 10 );
+  else if ( $upcoming->total_results > 10 ) {
+    $upcoming = array_slice( $upcoming->results, 0, 10 );
   }
-  foreach ( $upcomming as $movie ) {
-    $movie_saved = save_movie( $movie->id, $config );
+  
+  foreach ( $upcoming as $movie ) {
+    $movie_saved = save_movie( $movie->id, $config, true );
   }
-
-  $myfile = fopen( "response.json", "a" ) or die( "Unable to open file!" );
-  fwrite( $myfile, json_encode( $config ) );
-  fclose( $myfile );
-  chmod( 'response.json', 0777 );
 }
 
 
@@ -53,61 +47,43 @@ function fetch_API( $url ) {
 }
 
 
-function save_movie( $id, $config ) {
-  $url =  'https://api.themoviedb.org/3/movie/' . $id . '?api_key=dd749361bed88e6bbe29fb3e5396489c&language=en-US';
+function save_movie( $id, $config, $upcoming  ) {
+  $url =  API_URL . "movie/" . $id . "?api_key=" . API_KEY . "&language=en-US";
   $movie_details = fetch_API( $url );
 
+  $trailer = get_trailer( $id );
   $poster_url = $config->images->secure_base_url . $config->images->poster_sizes[3] . $movie_details->poster_path;
-
-  // movie trailer
-  $videos_url = "https://api.themoviedb.org/3/movie/" . $id . "/videos?api_key=dd749361bed88e6bbe29fb3e5396489c&language=en-US";
-  $videos = fetch_API( $videos_url );
-
-  $myfile = fopen( "response.json", "a" ) or die( "Unable to open file!" );
-  fwrite( $myfile, json_encode( $videos ) );
-  fclose( $myfile );
-  chmod( 'response.json', 0777 );
-
-  if ( isset($videos->results) ) {
-    foreach ( $videos->results as $video ) {
-      $myfile = fopen( "response.json", "a" ) or die( "Unable to open file!" );
-      fwrite( $myfile, json_encode( $video ) );
-      fclose( $myfile );
-      chmod( 'response.json', 0777 );
-
-      if ( $video->type == "Trailer" && $video->site == "YouTube" ) {
-        $trailer = $video;
-        break;
-      }
-    }
-  }
-  $trailer = !empty($trailer) ? 'https://www.youtube.com/watch?v=' . $trailer->key : '';
-
-  // cast
-  // reviews
+  $alternative_titles = get_alternative_titles( $id );
+  $cast = get_cast( $id, $config );
+  $reviews = get_reviews( $id );
 
   $movie_post = [
     'post_title' => $movie_details->original_title,
-    'guid' => $movie_details->id,
     'post_status' => 'publish',
     'post_type' => 'movie',
     'meta_input' => [
+      'id' => $id,
       'movie_trailer' => $trailer,
       'movie_poster' => $poster_url,
       'movie_genre' => json_encode( $movie_details->genres ),
-      'alternative_titles' => $movie_details->title,
+      'alternative_titles' => $alternative_titles,
       'overview' => $movie_details->overview,
       'production_companies' => json_encode( $movie_details->production_companies ),
       'release_date' => $movie_details->release_date,
       'original_language' => $movie_details->original_language,
-      'cast' => '',
+      'cast' => $cast,
       'popularity' => $movie_details->popularity,
-      'reviews' => ''
+      'reviews' => $reviews,
+      'upcoming' => $upcoming
     ]
   ];
-  wp_insert_post( $movie_post );
-
-  return true;
+  $post_id = wp_insert_post( $movie_post );
+  if ( !is_wp_error( $post_id ) ) {
+    return true;
+  } else {
+    //there was an error in the post insertion
+    return $post_id->get_error_message();
+  }
 
   /*
   Movie title
@@ -124,61 +100,58 @@ function save_movie( $id, $config ) {
   Reviews
   List of similar movies
   */
+}
 
-  /* movie_details
-  {
-    "adult": false,
-    "backdrop_path": "/h6SeTbpFRjx5NIppi6h5iXoFzBg.jpg",
-    "belongs_to_collection": null,
-    "budget": 0,
-    "genres": [
-      {
-        "id": 10752,
-        "name": "War"
-      },
-      {
-        "id": 18,
-        "name": "Drama"
-      },
-      {
-        "id": 36,
-        "name": "History"
+
+function get_trailer($id) {
+  $videos_url = API_URL . "movie/" . $id . "/videos?api_key=" . API_KEY . "&language=en-US";
+  $videos = fetch_API( $videos_url );
+  if ( isset($videos->results) ) {
+    foreach ( $videos->results as $video ) {
+      if ( $video->type == "Trailer" && $video->site == "YouTube" ) {
+        $trailer = $video;
+        break;
       }
-    ],
-    "homepage": "",
-    "id": 531876,
-    "imdb_id": "tt3833480",
-    "original_language": "en",
-    "original_title": "The Outpost",
-    "overview": "Based on true events, in this military thriller, a small unit of U.S. soldiers, alone at the remote Combat Outpost Keating, located deep in the valley of three mountains in Afghanistan, battles to defend against an overwhelming force of Taliban fighters in a coordinated attack. The Battle of Kamdesh, as it was known, was the bloodiest American engagement of the Afghan War in 2009 and Bravo Troop 3-61 CAV became one of the most decorated units of the 19-year conflict.",
-    "popularity": 31.936,
-    "poster_path": "/goEW6QqoFxNI2pfbpVqmXj2WXwd.jpg",
-    "production_companies": [],
-    "production_countries": [
-      {
-        "iso_3166_1": "BG",
-        "name": "Bulgaria"
-      },
-      {
-        "iso_3166_1": "US",
-        "name": "United States of America"
-      }
-    ],
-    "release_date": "2020-07-02",
-    "revenue": 0,
-    "runtime": 108,
-    "spoken_languages": [
-      {
-        "iso_639_1": "en",
-        "name": "English"
-      }
-    ],
-    "status": "In Production",
-    "tagline": "",
-    "title": "The Outpost",
-    "video": false,
-    "vote_average": 0,
-    "vote_count": 0
+    }
   }
-  */
+  $trailer = !empty( $trailer ) ? 'https://www.youtube.com/watch?v=' . $trailer->key : '';
+  return $trailer;
+}
+
+
+function get_alternative_titles( $id ) {
+  $alternative_titles_url = API_URL . "movie/" . $id . "/alternative_titles?api_key=" . API_KEY;
+  $alternative_titles = fetch_API( $alternative_titles_url );
+  $alternative_titles = !empty( $alternative_titles ) && isset( $alternative_titles->titles ) ? $alternative_titles->titles : [];
+  if( count( $alternative_titles ) ) {
+    foreach ( $alternative_titles as $alternative_title ) {
+      if ( $alternative_title->iso_3166_1 == 'US' ) {
+        $title = $alternative_title->title;
+        break;
+      }
+    }
+  }
+  $title = !empty( $title ) ? $title : '';
+  return $title;
+}
+
+
+function get_cast( $id, $config ) {
+  $cast_url = API_URL . "movie/" . $id . "/credits?api_key=" . API_KEY;
+  $cast = fetch_API( $cast_url );
+  $cast = !empty( $cast ) && isset( $cast->cast ) ? $cast->cast : [];
+  if( count( $cast ) ) {
+    foreach ( $cast as &$actor ) {
+      $actor->profile_path = $config->images->secure_base_url . $config->images->profile_sizes[1] . $actor->profile_path;
+    }
+  }
+  return json_encode( $cast );
+}
+
+
+function get_reviews( $id ) {
+  $reviews_url = API_URL . "movie/" . $id . "/reviews?api_key=" . API_KEY . "&language=en-US&page=1";
+  $reviews = fetch_API( $reviews_url );
+  $reviews = !empty( $reviews ) && isset( $reviews->results ) ? $reviews->results : [];
+  return json_encode( $reviews );
 }
